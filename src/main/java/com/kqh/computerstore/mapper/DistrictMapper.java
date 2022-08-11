f4a17c6ac419e1a692c2832c5f1079cb7d0152b5
kqh <1728209057@qq.com>
2022-08-11 09:55:30 +0800

    first

---
+++ src/main/java/com/kqh/computerstore/mapper/DistrictMapper.java
@@ -0,0 +1,20 @@
+package com.kqh.computerstore.mapper;
+
+import com.kqh.computerstore.entity.District;
+
+import java.util.List;
+
+/**
+ * @author hcxs1986
+ * @version 1.0
+ * @description: 实体类District对应的mapper接口
+ * @date 2022/7/14 15:14
+ */
+public interface DistrictMapper {
+
+    //根据父代号查询区域信息
+    List<District> queryDistrictByParent(String parent);
+
+    //根据code查询当前省市区的名称
+    String queryDistrictByCode(String code);
+}
